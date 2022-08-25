a59431c769a88fc5130b201e7a73982d6a32f3ef
Brady Stillman <bestillmanUNO@gmail.com>
2022-08-25 19:02:12 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Brady Stillman
+ *         bstillman@unomaha.edu
+ * Date: 2022/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  printf("Brady Stillman");
+
+  return 0;
+}
